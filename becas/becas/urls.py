fd34f72a0809946a638a729c85d74bf12995bbf2
Daf <ada_hack464@hackademy.mx>
2022-08-07 08:43:14 +0100

    Get becarios
    Primer  get para becarios

--- becas/becas/urls.py
+++ becas/becas/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from datosbecas.views import RetrieveBecario
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('becarios/', RetrieveBecario.as_view()),
 ]
